817d7cbe7989541a05bbb228ab226393c6484257
Rene Sebastian Pucheta <sebastian.pucheta@dirmod.com>
2024-10-06 15:18:52 -0300

    Added user check for JWT bearer.

--- src/modules/wallet/use-cases/wallet.use-case.ts
+++ src/modules/wallet/use-cases/wallet.use-case.ts
@@ -1,7 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { BalanceAccordingMovementService } from '../services/balance-according-movement.service';
-import { MarketDataService } from '@modules/marketdata/services/marketdata.service';
-import { InstrumentService } from '@modules/instruments/services/instrument.service';
 import { OrderService } from '@modules/orders/services/order.service';
 import { UserService } from '@modules/users/services/user.service';
 import { Order } from '@modules/orders/entities/order.entity';
@@ -9,35 +7,43 @@ import { GetInstrumentReturnService } from '../services/get-instrument-return.se
 import { SummarizeOrders } from '../services/summarize-orders.service';
 import { AssetDTO } from '../dtos/asset.dto';
 import { PortfolioDTO } from '../dtos/portfolio.dto';
+import { User } from '@modules/users/entities/user.entity';
 
 @Injectable()
 export class WalletUseCase {
   constructor(
     private readonly userService: UserService,
     private readonly orderService: OrderService,
-    private readonly marketDataService: MarketDataService,
-    private readonly instrumentService: InstrumentService,
     private readonly balanceAccordingMovementService: BalanceAccordingMovementService,
     private readonly getInstrumentReturnService: GetInstrumentReturnService,
     private readonly summarizeOrders: SummarizeOrders,
   ) {}
 
   async getWalletData(userId: number): Promise<PortfolioDTO> {
+    // Check if user exists
+    const user: User = await this.userService.findOne(userId);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
     const orders: Order[] = await this.orderService.findByUserId(userId);
 
-    // Uso del resumen
+    // Summarize orders so that order with same TICKER are not repeated
     const summarizedOrders: Order[] = await this.summarizeOrders.execute(
       orders,
     );
 
+    // Calculate and returns the investment return for every instrument
     const assets: AssetDTO[] = await this.getInstrumentReturnService.execute(
       summarizedOrders,
     );
 
+    // Calculate and returns current user balance according historical movements
     const available_cash = await this.balanceAccordingMovementService.execute(
       orders,
     );
 
+    // Calculate total balance by summing up available cash and investment returns
     const total_balance: number =
       available_cash +
       summarizedOrders.reduce((acc, summary) => acc + summary.price, 0);
